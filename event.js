ca77c76101d16b32350cfc03da761b137165aa2a
navi0105 <ivan000105@gmail.com>
2022-03-08 01:53:15 +0800

    first commit

--- event.js
+++ event.js
@@ -20,6 +20,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     //start simulation
     document.getElementById('start_btn').onclick = startSimulation;
+
+    //action list
+    document.getElementById('bpmChange_action').onclick = bpmModify;
+    document.getElementById('keyPress_action').onclick = pressKey;
+    document.getElementById('scratchPush_action').onclick = scratchPush;
+    document.getElementById('pressStartTwice_action').onclick = pressStartTwice;
+    document.getElementById('switchHsMode_action').onclick = switchHsMode;
 }, false);
 
 function fhsStartCheck(){
@@ -268,4 +275,35 @@ function nhsChange(){
 }
 
 //ACTION LIST
+function bpmModify(){
+    let next_BPM = Number(document.getElementById('BPM_after_changes').value);
+    hiSpeed_status.bpmChange(next_BPM)
+    hiSpeed_status.showStatus();
+}
 
+function pressKey(){
+    let key_type = Number(document.getElementById('key_type').value);
+    let key_count = Number(document.getElementById('key_count').value);
+    let press_times = Number(document.getElementById('press_times').value);
+
+    hiSpeed_status.pressKey(key_type, key_count, press_times);
+    hiSpeed_status.showStatus();
+}
+
+function scratchPush(){
+    let scr_movement = document.getElementById('scr_movement').value == ""? undefined: Number(document.getElementById('scr_movement').value);
+
+    hiSpeed_status.scratchPush(scr_movement);
+    hiSpeed_status.showStatus();
+}
+
+function pressStartTwice(){
+    hiSpeed_status.pressStartTwice();
+    hiSpeed_status.showStatus();
+
+}
+
+function switchHsMode(){
+    hiSpeed_status.switchHsMode();
+    hiSpeed_status.showStatus();
+}
